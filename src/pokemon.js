733b7718f5fb16e2aec5772ca94b8d3edca34a76
ybbarng <ybbarng@gmail.com>
2017-03-14 11:12:21 +0900

    iOS에서 도감 이미지가 어긋나는 것을 해결하기 위해 wrapper 도입

--- src/pokemon.js
+++ src/pokemon.js
@@ -135,7 +135,7 @@ function getMoveLinkText(move, moveName) {
 Pokemon.prototype.getPopupContents = function() {
   var despawnStr = this.getRemainTimeStr();
   return '<h2>' + this.name +
-      '<a href="http://pokemongo.inven.co.kr/dataninfo/pokemon/detail.php?code=' + this.pokemon_id + '" target="_blank" title="포켓몬도감에서 보기">' +
+      '<a href="http://pokemongo.inven.co.kr/dataninfo/pokemon/detail.php?code=' + this.pokemon_id + '" class="pokedex-wrapper" target="_blank" title="포켓몬도감에서 보기">' +
         '<img class="pokedex" src="/static/images/pokedex.png" alt="포켓몬도감에서 보기">' +
         '<img class="pokedex-pokemon-image" src="/static/images/pokemons/' + this.pokemon_id + '.png">' +
       '</a>' +
